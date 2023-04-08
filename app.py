9ab4044dec1e4befdc3e4b71593df5ba2e372d4c
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-04-08 02:44:04 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+
+# Import Libraries
+
+
+
+
+
+# Initilize the Flask
+
+
+
+# Route and define the index function to render the home.html.
+def index():
+	return render_template('home.html')
+
+
+
+# Call the app.run()
+
+	<label>PASSWORD</label>
+	<input type ="password" placeholders="password" name="password"><br>
+
+
+
+
+
+
+<input  class="btn btn-primary btn-lg" type = "submit" value="Get OTP" ><br><br>
